6609a6fe5241a708754f654e0e0297bbd812e3de
Baashe <baashe@Baashes-MacBook-Air.local>
2025-08-13 19:33:54 +0300

    i have added empty fragments in a h2 line

--- abdulrahman-react-app/src/components/Greeting.jsx
+++ abdulrahman-react-app/src/components/Greeting.jsx
@@ -1,8 +1,13 @@
+import { Fragment } from "react";
+
 function Greeting(props) {
   return (
-    <h1>
-      hello, {props.name} {props.surname}!
-    </h1>
+    <>
+      <h1>
+        hello, {props.name} {props.surname}!
+      </h1>
+      <h2>this text was not there</h2>
+    </>
   );
 }
 
